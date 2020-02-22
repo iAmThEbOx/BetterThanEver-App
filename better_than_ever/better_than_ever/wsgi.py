4b075f51f01a70f77dbbe5028d81b0f548cfa07d
theadityat24 <52205250+theadityat24@users.noreply.github.com>
2020-02-21 22:12:41 -0600

    Initial Commit and added CustomUser model

---
+++ better_than_ever/better_than_ever/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for better_than_ever project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'better_than_ever.settings')
+
+application = get_wsgi_application()
---
+++ better_than_ever/users/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class CustomUser(AbstractUser):
+    height = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(10), MaxValueValidator(120)]
+    )
+    weight = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(2), MaxValueValidator(1500)]
+    )
+    age = models.PositiveIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(150)]
+    )
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+    )
+    profile_pic = models.ImageField(height_field=200, width_field=200)
+    sleep_time = models.TimeField()
+    sleep_duration = models.TimeField()
+    balance = models.PositiveIntegerField()
+    address = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=14)
+    challenges_completed = models.ManyToManyField('FitnessChallenge', on_delete=models.CASCADE)
+
+
+
