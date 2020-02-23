0b006e30dc650aea55e120acd59fe483dcc06315
theadityat24 <52205250+theadityat24@users.noreply.github.com>
2020-02-23 11:42:21 -0600

    Added Fitness Challenge models

--- better_than_ever/users/admin.py
+++ better_than_ever/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, FitnessChallenge, FitnessChallengeProgress
 
 
 admin.site.register(Profile)
+admin.site.register(FitnessChallenge)
+admin.site.register(FitnessChallengeProgress)
--- better_than_ever/users/models.py
+++ better_than_ever/users/models.py
@@ -5,6 +5,8 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from datetime import datetime, timedelta
+
 class Profile(models.Model):
     height = models.PositiveSmallIntegerField(
         validators=[MinValueValidator(10), MaxValueValidator(120)],
@@ -27,10 +29,11 @@ class Profile(models.Model):
     )
     sleep_time = models.TimeField(null=True)
     sleep_end = models.TimeField(null=True)
-    balance = models.PositiveIntegerField(default=0)
+    balance = models.DecimalField(default=0, max_digits=20, decimal_places=2)
     address = models.CharField(max_length=200, blank=True)
     phone_number = models.CharField(max_length=14, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    revenue = models.DecimalField(default=0, max_digits=20, decimal_places=2)
 
     def __str__(self):
         return f'{self.user.username}\'s Profile'
@@ -45,8 +48,32 @@ def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 
-class FitnessChallengeProgress(models.Model):
-    pass
 
 class FitnessChallenge(models.Model):
-    pass
+    challenge_text = models.TextField(max_length=300)
+    worth = models.DecimalField(
+        default=0, max_digits=20, decimal_places=2
+    )
+    challenge_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.challenge_text}'
+
+
+class FitnessChallengeProgress(models.Model):
+    fitness_challenge = models.ForeignKey(FitnessChallenge, on_delete=models.CASCADE)
+    progress = models.DecimalField(
+        max_digits=8, decimal_places=7, default=0
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_accepted = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField(
+        default=datetime.utcnow() + timedelta(days=1)
+    )
+
+    def __str__(self):
+        return f'{self.fitness_challenge.challenge_text}: {self.user.username}\'s at {self.progress}'
+
+
+    
+
