9cf6a88be6e3c252420011ec7cb5e3156f7b2bb4
iAmThEbOx <60986075+iAmThEbOx@users.noreply.github.com>
2020-03-04 16:17:01 -0600

    Update models.py

--- better_than_ever/recipes/models.py
+++ better_than_ever/recipes/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
-
+class Food(models.Model):
+    title = models.CharField(max_length=140)
+    description = models.CharField(max_length=300)
+    link = models.CharField(max_length=500)
+    prep_time = models.CharField(max_length=50)
 
 # Create your models here.
 class Recipe(models.Model):
     CATEGORY_CHOICES = [
-        ('BR', 'Breakfast'),
-        ('LU', 'Lunch'),
-        ('DI', 'Dinner'),
-        ('SN', 'Snack')
+        ('FR', 'Fruits'),
+        ('VE', 'Vegetables'),
+        ('ME', 'Meats'),
+        ('GR', 'Grains'),
+        ('CA', 'Carbohydrates'),
+        ('DE', 'Desserts')
     ]
     category = models.CharField(
         max_length=2, choices=CATEGORY_CHOICES
@@ -27,10 +33,8 @@ class NutritionInfo(models.Model):
     calories = models.PositiveIntegerField()
     added_sugars = models.PositiveIntegerField()
 
-class RecipeInfo(models.Model):
-    prep_time = models.CharField(max_length=140)
+class RecipeInfo(models.Model)
     active_time = models.CharField(max_length=140)
-    cook_time = models.CharField(max_length=140)
     nutrition_info = models.OneToOneField(NutritionInfo, on_delete=models.CASCADE)
     recipe = models.OneToOneField(Recipe, on_delete=models.CASCADE)
 
